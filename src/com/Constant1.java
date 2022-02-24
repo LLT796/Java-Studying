7aa3e64a2fde7c3f5cc4edc41e003d782734ed61
LangtaoLiu <13225516696@163.com>
2022-02-24 09:50:19 +0800

    Big Integer Test

---
+++ src/com/Constant1.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Constant1 {
+    public static void main(String[] args)
+    {
+        final double CPI=2.54;
+        double pW=8.5;
+        double pH=11;
+        System.out.println("Paper size in centimeters: "+pW*CPI+" by "+pH*CPI);
+    }
+}
---
+++ src/com/Constant2.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Constant2 {
+    public static final double CPI=2.54;
+    public static void main(String[] args)
+    {
+        double pW=8.5;
+        double pH=11;
+        System.out.println("Paper size in centimeters: "+pW*CPI+" by "+pH*CPI);
+    }
+}
